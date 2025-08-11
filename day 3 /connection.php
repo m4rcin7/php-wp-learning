1f1a4064a7caa58c36adb562a8f2743e812c7613
Marcin Kopatys <mrku1337@gmail.com>
2025-08-11 16:59:03 +0200

    connection with db

--- day 3 /connection.php
+++ day 3 /connection.php
@@ -1,2 +1,22 @@
-<?php 
+<?php
 // connection with db
+
+$host = 'localhost';
+$dbname = 'php_wp_learning';
+$username = 'root';
+$password = '';
+
+$conn = mysqli_connect($host, $username, $password, $dbname);
+
+if (!$conn) {
+    die("Error db connection: " . mysqli_connect_error());
+}
+
+mysqli_set_charset($conn, "utf8");
+
+function close_connection($conn)
+{
+    mysqli_close($conn);
+}
+
+?>
