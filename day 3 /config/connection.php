5194043226223547a7193d4988ac6c451c451597
Marcin Kopatys <mrku1337@gmail.com>
2025-08-12 18:57:52 +0200

    file configuration

---
+++ day 3 /config/connection.php
@@ -0,0 +1,17 @@
+<?php
+// connection with db
+
+$host = 'localhost';
+$dbname = 'php_wp_learning';
+$username = 'root';
+$password = '';
+
+$conn = new mysqli($host, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$conn->set_charset("utf8");
+
+?>
