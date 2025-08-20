ce2578a0de5602f7a0bc22e21988e6628a7eb626
Marcin Kopatys <mrku1337@gmail.com>
2025-08-20 19:55:49 +0200

    simplification of the structure

--- day4/home.php
+++ day4/home.php
@@ -17,9 +17,9 @@ session_start();
         <div><img src="https://cdn.pixabay.com/photo/2015/12/04/07/55/logodesign-1076200_1280.png" alt="logo-agency"></div>
         <nav>
             <a href="./home.php">Home</a>
-            <a href="./home.php">About us</a>
-            <a href="./home.php">Blog</a>
-            <a href="./home.php">Contact</a>
+            <a href="./about.php">About us</a>
+            <a href="./blog.php">Blog</a>
+            <a href="./contact.php">Contact</a>
             <a href="./login.php">Log in</a>
         </nav>
     </header>
