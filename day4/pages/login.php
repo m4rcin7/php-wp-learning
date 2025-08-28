a36ff30a41e327c6e24c6cf5f9ded61acb978e09
Marcin Kopatys <mrku1337@gmail.com>
2025-08-28 15:18:36 +0200

    login styles page

--- day4/pages/login.php
+++ day4/pages/login.php
@@ -1,15 +1,15 @@
 <form class="login-container">
     <h2>Login</h2>
-    <div>
-        <label for="email">Email:</label>
-        <input type="email" id="email" name="email" placeholder="Enter you email">
+    <div class="login-group">
+        <label class="login-label" for="email">Email:</label>
+        <input class="login-input" type="email" id="email" name="email" placeholder="Enter you email">
     </div>
-    <div>
-        <label for="password">Password:</label>
-        <input type="password" id="password" name="password" placeholder="Enter you password">
+    <div class="login-group">
+        <label class="login-label" for="password">Password:</label>
+        <input class="login-input" type="password" id="password" name="password" placeholder="Enter you password">
     </div>
-    <button type="submit" class="btn-login">Login</button>
-    <div>
+    <button type="submit" class="login-btn">Login</button>
+    <div class="login-footer">
         <a href="#">Register</a>
     </div>
 </form>
