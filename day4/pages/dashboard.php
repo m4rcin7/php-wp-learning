485f397d849d1307bc653500512457371745ec93
Marcin Kopatys <mrku1337@gmail.com>
2025-09-02 18:55:44 +0200

    content dashboard

---
+++ day4/pages/dashboard.php
@@ -0,0 +1,9 @@
+<section>
+    <h2>Admin panel</h2>
+    <div>
+        <button>Add post</button>
+        <button>View all posts</button>
+        <button>Add User</button>
+        <button>View all sers</button>
+    </div>
+</section>
